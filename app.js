65b69a39e45246739aa00cc5ea6a88d732b283b7
farihaorna23 <farihaorna@gmail.com>
2022-08-30 19:37:16 -0400

    Finished Exercise

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // TODO: Enable strict mode
-
+"use strict";
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
+  try {
+    const resultData = JSON.parse(data);
+    return resultData;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
   // TODO: if an exception is raised
   // print the error to the console
   // and return null
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
+//The JSON.stringify() method converts a JavaScript value to a JSON string,
 let passData = JSON.stringify({ success: true });
-
+console.log(passData);
 console.log(parseToJSON(failData));
 
 console.log(parseToJSON(passData));
